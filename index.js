5e14df57583d8dbeb65fe3abb295bb99e058b42f
Jeremy Davidson <jeremyfdavidson@gmail.com>
2024-11-15 19:00:54 -0800

    Moved rules to shared file like eslint-9 branch

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict"
 
-// // If export module: "test": node --experimental-vm-modules node_modules/.bin/jest
-// export default {
+const sharedRules = require("./index.rules")
+
 module.exports = {
   "parser": "@typescript-eslint/parser",
   "env": {
@@ -16,61 +16,7 @@ module.exports = {
   "extends": [
     "google",
   ],
-  "rules": {
-    "@stylistic/ts/indent": [
-      "error",
-      2,
-      {
-        "ignoredNodes": [
-          "PropertyDefinition[decorators]",
-          "CallExpression[arguments]",
-        ],
-      },
-    ],
-    "@stylistic/ts/quotes": [
-      2,
-      "double",
-    ],
-    "indent": ["error", 2],
-    "max-len": ["error", {
-      code: 180,
-      tabWidth: 2,
-      ignoreUrls: true,
-    }],
-    "new-cap": [
-      "error",
-      {
-        "capIsNew": false,
-      },
-    ],
-    "no-multi-str": "off",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": [
-      "error",
-      "always",
-    ],
-    "padded-blocks": [
-      "error",
-      {
-        "classes": "always",
-      },
-    ],
-    "quotes": [
-      2,
-      "double",
-    ],
-    "require-jsdoc": "off",
-    "semi": [
-      "error",
-      "never",
-    ],
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
-    // "sort-imports": [
-    //   "error"
-    // ],
-    "valid-jsdoc": "off",
-  },
+  "rules": sharedRules,
   "overrides": [
     {
       "files": ["*.json", "*.jsonc"],
